2697fa3da1ef786e977dfd16dc8ef60cb08c3f2c
Rahuljha5671 <84356368+Rahuljha5671@users.noreply.github.com>
2022-06-21 05:31:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const date = require(__dirname + "/date.js");
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set('views',__dirname+"/views");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
